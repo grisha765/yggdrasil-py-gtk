74c973ce65923dd5baafa29ea21d31f9b7cc4ae7
Grisha <grisha765@tuta.io>
2025-03-15 00:31:52 +0300

    [-] general func

--- yggui/func/config.py
+++ yggui/func/config.py
@@ -1,56 +1,13 @@
-import json
-from pathlib import Path
-from core.common import Default
+import subprocess
 
-def load_config_file():
-    config_file = Path("config.json")
-    if not config_file.exists():
-        default_config = {
-            "ygg_path": {
-                "params": {
-                    "param": Default.ygg_path,
-                    "use_config": False
-                }
-            }
-        }
-        with config_file.open("w", encoding="utf-8") as f:
-            json.dump(default_config, f, ensure_ascii=False, indent=4)
-        return default_config
-    with config_file.open("r", encoding="utf-8") as f:
-        config = json.load(f)
-    return config
+from yggui.core.common import Default
 
-def load_config_params():
-    config = load_config_file()
-    config = config
-    config_ygg_path = config["ygg_path"]["params"]["param"]
-    config_ygg_path_check = config["ygg_path"]["params"]["use_config"]
-    ygg_path = config_ygg_path if config_ygg_path_check else Default.ygg_path
-    return ygg_path, config_ygg_path, config_ygg_path_check
-
-def save_config_file(config):
-    config_file = Path("config.json")
-    with config_file.open("w", encoding="utf-8") as f:
-        json.dump(config, f, ensure_ascii=False, indent=4)
-
-def ygg_path_check(self, button):
-    self.ygg_path_input.set_sensitive(self.ygg_path_check.get_active())
-
-def save_config(self, button):
-    ygg_path_check = self.ygg_path_check.get_active()
-    self.ygg_path = self.ygg_path_input.get_text()
-    print(f"Using ygg_path parameter: {self.ygg_path}")
-    print(f"Check box ygg_path: {ygg_path_check}")
-    config = {
-        "ygg_path": {
-            "params": {
-                "param": self.ygg_path,
-                "use_config": ygg_path_check
-            }
-        }
-    }
-    save_config_file(config)
-    self.ygg_path, self.config_ygg_path, self.config_ygg_path_check = load_config_params()
+def create_config():
+    if not Default.config_path.exists():
+        print(f"The {Default.config_file} file was not found. Create it...")
+        with open(str(Default.config_path), "w") as f:
+            cmd = [Default.ygg_path, "-genconf", "-json"]
+            subprocess.run(cmd, stdout=f, check=True)
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")

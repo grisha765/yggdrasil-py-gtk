e4cb30e14c7198a5892af1adcb66a181a7ddc6e0
Grisha <grisha765@tuta.io>
2025-03-15 18:21:25 +0300

    [u] add path file

--- yggui/core/common.py
+++ yggui/core/common.py
@@ -5,3 +5,5 @@ class Default:
     ygg_path = shutil.which('yggdrasil')
     config_file = 'config.json'
     config_path = Path(config_file)
+    ui_file = 'yggui/ui/ui.ui'
+    css_file = 'yggui/ui/ui.css'

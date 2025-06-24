55a3065c622ae141ada82a3761962b7e1f39f034
Grisha <grisha765@tuta.io>
2025-06-25 01:21:52 +0300

    [u] gen admin socket in config

--- yggui/func/config.py
+++ yggui/func/config.py
@@ -1,6 +1,22 @@
-import subprocess
+import subprocess, json
 
 from yggui.core.common import Default
+
+
+def _ensure_admin_socket_is_set() -> None:
+    try:
+        with open(Default.config_path, "r+", encoding="utf-8") as handle:
+            cfg = json.load(handle)
+            if cfg.get("AdminListen") == f'unix://{Default.admin_socket}':
+                return
+
+            cfg["AdminListen"] = f'unix://{Default.admin_socket}'
+            handle.seek(0)
+            json.dump(cfg, handle, indent=2)
+            handle.truncate()
+    except Exception:
+        pass
+
 
 def create_config():
     if not Default.config_path.exists():
@@ -8,6 +24,8 @@ def create_config():
         with open(str(Default.config_path), "w") as f:
             cmd = [Default.ygg_path, "-genconf", "-json"]
             subprocess.run(cmd, stdout=f, check=True)
+    _ensure_admin_socket_is_set()
+
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")

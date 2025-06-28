e9823739b8526f278f01ea889e87e25c4068fbed
Grisha <grisha765@tuta.io>
2025-06-28 21:17:10 +0300

    [u] app id

--- yggui/__main__.py
+++ yggui/__main__.py
@@ -4,6 +4,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from yggui.core.window import MyApp
 
 if __name__ == '__main__':
-    app = MyApp(application_id="com.example.GtkApplication")
+    app = MyApp(application_id="io.github.grisha765.yggdrasil-go-gtk")
     app.run(sys.argv)
 

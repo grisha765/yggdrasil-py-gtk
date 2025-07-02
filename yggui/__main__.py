047f7e7555d891efc0612ddde1d0e07d285f4022
Grisha <grisha765@tuta.io>
2025-07-02 17:32:46 +0300

    [f] fix main launcher for new build

--- yggui/__main__.py
+++ yggui/__main__.py
@@ -1,27 +1,32 @@
 import os, sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from yggui.core.common import Binary
-
-if Binary.ygg_path is None:
-    raise FileNotFoundError(
-        "The 'yggdrasil' executable was not found in your PATH. "
-        "Please install Yggdrasil or adjust your PATH environment "
-        "variable accordingly."
-    )
-
-if Binary.yggctl_path is None:
-    raise FileNotFoundError(
-        "The 'yggdrasilctl' executable was not found in your PATH. "
-        "Please install Yggdrasil or adjust your PATH environment "
-        "variable accordingly."
-    )
-
 from yggui.core.window import MyApp
 
-if __name__ == '__main__':
+
+def _ensure_prerequisites():
+    if Binary.ygg_path is None:
+        raise FileNotFoundError(
+            "The 'yggdrasil' executable was not found in your PATH. "
+            "Please install Yggdrasil or adjust your PATH environment "
+            "variable accordingly."
+        )
+
+    if Binary.yggctl_path is None:
+        raise FileNotFoundError(
+            "The 'yggdrasilctl' executable was not found in your PATH. "
+            "Please install Yggdrasil or adjust your PATH environment "
+            "variable accordingly."
+        )
+
+
+def main():
+    _ensure_prerequisites()
     app = MyApp(
         application_id="io.github.grisha765.yggdrasil-go-gtk"
     )
     app.run(sys.argv)
 
+
+if __name__ == "__main__":
+    sys.exit(main())
